ab7b52d9265dd9cf6e55ecbb4b7b8127804de3e7
xuanbach174 <xuanbach174@gmail.com>
2026-01-10 15:32:52 +0700

    api test

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  //testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -40,10 +40,19 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
+      testDir: 'tests_ui',
       use: { 
         ...devices['Desktop Chrome'], 
         headless: false, 
       },
+    },
+
+    {
+      name: 'apiTest',
+      testDir: 'tests_api',
+      use: { 
+        headless: true,
+      }
     },
 
     /* Test against mobile viewports. */
